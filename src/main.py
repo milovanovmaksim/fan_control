d0d9fc56ee61b2d4b62bb18d525782eed037738b
milovanovmaksim <milovanov160386@gmail.com>
2024-01-27 15:21:26 +0300

    change temp_max

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from src.temperature import Temperature
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.ERROR)
     temp_min = 40
-    temp_max = 70
+    temp_max = 80
     delay = 3
     pin = 14
     path = "/sys/class/thermal/thermal_zone0/temp"
