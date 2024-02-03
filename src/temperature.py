fb53e7e38f5d93c9cc07e9de88d9e3d3e141a263
milovanovmaksim <milovanov160386@gmail.com>
2024-02-03 23:01:12 +0300

    add try except

--- src/temperature.py
+++ src/temperature.py
@@ -1,4 +1,4 @@
-from logging import debug
+from logging import error
 from typing import Union
 
 
@@ -9,14 +9,18 @@ class Temperature():
     def __init__(self, path: str):
         self._path = path
 
-    def temperature(self) -> Union[float, ValueError]:
+    def temperature(self) -> Union[int, Exception]:
         """
         Читает температуру CPU Raspberry pi4 из файла.
         """
-        with open(self._path) as reader:
-            try:
-                temeperature = int(reader.readline())
-                return round(temeperature / 1000)
-            except ValueError as e:
-                debug(f"Temperature.temperature | {e}")
-                return e
+        try:
+            with open(self._path, "r") as reader:
+                try:
+                    temeperature = int(reader.readline())
+                    return round(temeperature / 1000)
+                except ValueError as e:
+                    error(f"Temperature.temperature | {e}")
+                    return e
+        except Exception as e:
+            error(f"Temperature.temperature | {e}")
+            return e
